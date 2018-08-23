e483f3a1630d9c7d6099fb9b819d4848268b0aa4
Sampuccino <Sampuccino@gmail.com>
2018-08-23 15:12:45 -0700

    Did a basic UI to start developing the app.

--- Index.php
+++ Index.php
@@ -1,7 +1,86 @@
 <?php include "resources/includes/header.php" ?>
 
+<div class="container">
+
+  <div class="d-flex justify-content-center m-3">
+    <h3 class="Lobster-Two">
+      <span>
+        <img src="resources/img/store.svg" width="40em" alt="">
+      </span>Exotic Store</h3>
+  </div>
+
+    <div class="dropdown open mt-5 d-flex flex-row-reverse">
+      <button class="btn btn-light dropdown-toggle" type="button" id="triggerId" data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false">
+        <img src="resources/img/cart.svg" width="30" alt="">
+      </button>
+      <div class="dropdown-menu" aria-labelledby="triggerId">
+        <p class="p-3 Yantram">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ad atque aut autem dignissimos dolor
+          enim et inventore ipsam iure maxime molestias nesciunt numquam provident rem repellendus unde vel.
+          Assumenda.</p>
+      </div>
+    </div>
+
+</div>
+
+<!-- Items Available -->
+<div class="container animated fadeInUp" style="margin-top: 5rem">
+  <div class="row">
+
+    <div class="col-md-8 col-12 mb-5" id="itemForSale">
+      <h4 class="Lobster-Two-700">Ferrari<u class="ml-2">$250,000</u></h4>
+      <img src="resources/img/ferrari.jpg" class="img-fluid rounded" alt="">
+      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+        distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
+        maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
+        temporibus vitae voluptatum.</p>
+      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+    </div>
+
+    <div class="col-md-4 col-12 mb-5" id="itemForSale">
+      <h4 class="Lobster-Two-700">Tiger<u class="ml-2">$10,000</u></h4>
+      <img src="resources/img/tiger.jpg" class="img-fluid rounded" alt="">
+      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+        distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
+        maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
+        temporibus vitae voluptatum.</p>
+      <button class="btn btn-primary btn-block">Add To Cart</button>
+    </div>
+
+    <div class="col-md-3 col-12 mb-5" id="itemForSale">
+      <h4 class="Lobster-Two-700">Flower<u class="ml-2">$2,500</u></h4>
+      <img src="resources/img/flower.jpg" class="img-fluid rounded" alt="">
+      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+        distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
+        maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
+        temporibus vitae voluptatum.</p>
+      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+    </div>
+
+    <div class="col-md-6 col-12 mb-5" id="itemForSale">
+      <h4 class="Lobster-Two-700">Vacation<u class="ml-2">$4,500</u></h4>
+      <img src="resources/img/vacation.jpg" class="img-fluid rounded" alt="">
+      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+        distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
+        maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
+        temporibus vitae voluptatum.</p>
+      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+    </div>
+
+    <div class="col-md-3 col-12 mb-5" id="itemForSale">
+      <h4 class="Lobster-Two-700">Parrot<u class="ml-2">$3,250</u></h4>
+      <img src="resources/img/bird.jpg" class="img-fluid rounded" alt="">
+      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+        distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
+        maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
+        temporibus vitae voluptatum.</p>
+      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+    </div>
 
 
+  </div>
+</div>
 
 
 
--- resources/includes/header.php
+++ resources/includes/header.php
@@ -5,7 +5,7 @@
   <meta name="viewport"
         content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Local Storage Messenger</title>
+  <title>Local Storage Shopping Cart</title>
 
   <!--Bootstrap 4-->
   <link rel="stylesheet" href="./resources/css/bootstrap.min.css">
@@ -15,6 +15,8 @@
   <link rel="stylesheet" href="./resources/css/style.css">
   <link rel="stylesheet" href="https://printjs-4de6.kxcdn.com/print.min.css">
   <script src="https://printjs-4de6.kxcdn.com/print.min.js"></script>
+  <!--Fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Lobster+Two:400,400i,700|Yantramanav:400,500,700,900" rel="stylesheet">
 
 </head>
 
