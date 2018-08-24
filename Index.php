3ea95001d4a7a071ea5dd450fca999c74525608d
Sampuccino <Sampuccino@gmail.com>
2018-08-23 19:25:09 -0700

    Adjusted the styling and responsiveness for the shopping cart.

--- Index.php
+++ Index.php
@@ -15,10 +15,13 @@
               aria-expanded="false">
         <img src="resources/img/cart.svg" width="30" alt="">
       </button>
-      <div class="dropdown-menu" aria-labelledby="triggerId">
-        <p class="p-3 Yantram">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus, ad atque aut autem dignissimos dolor
-          enim et inventore ipsam iure maxime molestias nesciunt numquam provident rem repellendus unde vel.
-          Assumenda.</p>
+      <div class="dropdown-menu col-12 animated fadeIn" aria-labelledby="triggerId">
+        <div id="cart-items">
+          <!--Added Products Here-->
+        </div>
+        <div class="col-8 offset-2">
+          <div class="btn btn-outline-danger btn-block m-3 Yantram-700" id="clear-cart">Clear Cart</div>
+        </div>
       </div>
     </div>
 
@@ -26,56 +29,56 @@
 
 <!-- Items Available -->
 <div class="container animated fadeInUp" style="margin-top: 5rem">
-  <div class="row">
+  <div class="row" id="product-list">
 
     <div class="col-md-8 col-12 mb-5" id="itemForSale">
-      <h4 class="Lobster-Two-700">Ferrari<u class="ml-2">$250,000</u></h4>
+      <h4 class="Lobster-Two-700">Ferrari <u class="ml-2 price">$250,000</u></h4>
       <img src="resources/img/ferrari.jpg" class="img-fluid rounded" alt="">
-      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+      <p class="Yantram mt-3 mb-3 description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
         distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
         maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
         temporibus vitae voluptatum.</p>
-      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+      <button class="btn btn-primary btn-block add-to-cart Yantram-900" data-id="1" >Add To Cart</button>
     </div>
 
     <div class="col-md-4 col-12 mb-5" id="itemForSale">
-      <h4 class="Lobster-Two-700">Tiger<u class="ml-2">$10,000</u></h4>
+      <h4 class="Lobster-Two-700">Tiger <u class="ml-2 price">$10,000</u></h4>
       <img src="resources/img/tiger.jpg" class="img-fluid rounded" alt="">
-      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+      <p class="Yantram mt-3 mb-3 description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
         distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
         maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
         temporibus vitae voluptatum.</p>
-      <button class="btn btn-primary btn-block">Add To Cart</button>
+      <button class="btn btn-primary btn-block add-to-cart Yantram-900" data-id="2">Add To Cart</button>
     </div>
 
     <div class="col-md-3 col-12 mb-5" id="itemForSale">
-      <h4 class="Lobster-Two-700">Flower<u class="ml-2">$2,500</u></h4>
+      <h4 class="Lobster-Two-700">Flower <u class="ml-2 price">$2,500</u></h4>
       <img src="resources/img/flower.jpg" class="img-fluid rounded" alt="">
-      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+      <p class="Yantram mt-3 mb-3 description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
         distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
         maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
         temporibus vitae voluptatum.</p>
-      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+      <button class="btn btn-primary btn-block add-to-cart Yantram-900" data-id="3">Add To Cart</button>
     </div>
 
     <div class="col-md-6 col-12 mb-5" id="itemForSale">
-      <h4 class="Lobster-Two-700">Vacation<u class="ml-2">$4,500</u></h4>
+      <h4 class="Lobster-Two-700">Vacation <u class="ml-2 price">$4,500</u></h4>
       <img src="resources/img/vacation.jpg" class="img-fluid rounded" alt="">
-      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+      <p class="Yantram mt-3 mb-3 description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
         distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
         maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
         temporibus vitae voluptatum.</p>
-      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+      <button class="btn btn-primary btn-block add-to-cart Yantram-900" data-id="4">Add To Cart</button>
     </div>
 
     <div class="col-md-3 col-12 mb-5" id="itemForSale">
-      <h4 class="Lobster-Two-700">Parrot<u class="ml-2">$3,250</u></h4>
+      <h4 class="Lobster-Two-700">Parrot <u class="ml-2 price">$3,250</u></h4>
       <img src="resources/img/bird.jpg" class="img-fluid rounded" alt="">
-      <p class="Yantram mt-3 mb-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
+      <p class="Yantram mt-3 mb-3 description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium atque aut commodi cupiditate deleniti
         distinctio doloremque dolorum, ea eius enim et eum ex excepturi expedita id impedit inventore ipsa ipsam itaque
         maxime minus nam neque nisi odio pariatur praesentium quam quod reiciendis rerum saepe sapiente, sit suscipit
         temporibus vitae voluptatum.</p>
-      <button class="btn btn-primary btn-block" style="border-radius: 0 !important;">Add To Cart</button>
+      <button class="btn btn-primary btn-block add-to-cart Yantram-900" data-id="5">Add To Cart</button>
     </div>
 
 
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,75 @@
 console.log('Script loaded in!');
+
+/* Variables */
+const products = $('#product-list'), $cartItem = $('#cart-items');
+
+loadEventListener();
+
+function loadEventListener() {
+
+  //New product added to cart
+  products.click(buyProduct);
+}
+
+function buyProduct(e) {
+  e.preventDefault();
+  // console.log(e.target);
+
+  //Use delegation to find course that was added
+  if(e.target.classList.contains('add-to-cart')){
+    // console.warn('Item Added');
+    const aProduct = e.target.parentElement;
+
+    getProductInformation(aProduct);
+  }
+
+}
+
+function getProductInformation(someProduct) {
+  //Product Object
+  const productInfo = {
+    image: someProduct.querySelector('img').src,
+    title: someProduct.querySelector('h4').textContent,
+    price: someProduct.querySelector('.price').textContent,
+    description: someProduct.querySelector('p').innerText,
+    id: someProduct.querySelector('button').getAttribute('data-id')
+  };
+
+  //Insert product into shopping car
+  addToCart(productInfo);
+
+  // console.log(productInfo.image);
+  console.log(productInfo);
+
+}
+
+function addToCart(productObject) {
+  // const $cart = `<div class="row mt-2" data-id="${productObject.id}">
+  //                <div class="col-5 offset-1">
+  //                 <img id="cart-Img" class="img-fluid rounded" src="${productObject.image}" width="100" alt="">
+  //                </div>
+  //                <div class="col-5 text-center Lobster-Two-700" style="font-size: 1.15rem">
+  //                 ${productObject.title}
+  //                 <div class="row text-center mx-auto">
+  //                   <button class="btn btn-light btn-block">
+  //                     <img src="./resources/img/delete.svg" width="20" alt="">
+  //                   </button>
+  //                 </div>
+  //                </div></div> `;
+
+  const $cart =
+    `
+    <div class="col-8 offset-2 flex-row-reverse text-center p-3" data-id="${productObject.id}">
+    <img id="cart-Img" class="img-fluid rounded mb-2" src="${productObject.image}" width="100" alt="">
+    <h4 class="text-center Yantram-700">${productObject.title}.</h4>
+    <p class="font-weight-light">${productObject.description.substr(0,80)}</p>
+    <button class="btn btn-light btn-block">
+      <img src="./resources/img/delete.svg" width="20" alt="">
+    </button>
+    </div> 
+    `;
+
+
+  $cartItem.append($cart);
+
+}
